7ef114f44db3eebdbfd0218576eac488a0b43d52
kpman <s92f002@hotmail.com>
2021-05-24 16:00:00 +0800

    feat(seo): enhance SEO

--- components/seo.js
+++ components/seo.js
@@ -3,28 +3,32 @@ import React from 'react';
 
 import config from '../config';
 
-const SEO = ({ title, description }) => {
-  const metaDescription = description || config.description;
-
-  const defaultTitle = config.title;
-  const renderTitle = title ? `${title} | ${defaultTitle}` : defaultTitle;
+const SEO = ({
+  title: titleFromProps,
+  description = config.description,
+  ogImageUrl,
+  slug,
+}) => {
+  const siteName = config.title;
+  const title = titleFromProps ? `${titleFromProps} | ${siteName}` : siteName;
+  const url = slug ? `${config.siteUrl}${slug}` : config.siteUrl;
 
   return (
     <Head>
-      <title>{renderTitle}</title>
+      <title>{title}</title>
 
       <meta name="viewport" content="width=device-width" />
-      <meta name="description" content={metaDescription} />
+      <meta name="description" content={description} />
 
+      <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
-      <meta property="og:url" content={config.siteUrl} />
-      <meta property="og:site_name" content={defaultTitle} />
-      <meta property="og:description" content={metaDescription} />
+      <meta property="og:description" content={description} />
+      {ogImageUrl && <meta property="og:image" content={ogImageUrl} />}
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
+      <meta name="twitter:description" content={description} />
 
       <link
         rel="apple-touch-icon"
