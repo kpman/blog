e5f5bc251b56eb587c20666ac49d9d34ca33b128
kpman <s92f002@hotmail.com>
2021-08-08 22:14:11 +0800

    feat: add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import { GA_4_TRACKING_ID } from '../lib/constants';
+
 import '../styles/index.css';
 import '../styles/prism-coy.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', GA_4_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+import { GA_4_TRACKING_ID } from '../lib/constants';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_4_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_4_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
