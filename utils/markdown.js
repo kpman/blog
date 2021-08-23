727014a40ade2475d219c70745b5a6facef0cdc4
kpman <s92f002@hotmail.com>
2021-08-23 20:36:53 +0800

    chore: bump remark related deps

--- utils/markdown.js
+++ utils/markdown.js
@@ -1,6 +1,6 @@
 import html from 'remark-html';
 import prism from 'remark-prism';
-import remark from 'remark';
+import { remark } from 'remark';
 
 const markdownToHtml = async (markdown) => {
   const result = await remark().use(prism).use(html).process(markdown);
